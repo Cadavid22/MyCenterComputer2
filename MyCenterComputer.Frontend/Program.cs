c22e380355902e075a539c34b398f4a936692ca3
Jorge Cadavid <cadavidjorge10@gmail.com>
2023-06-11 13:30:55 -0500

    Changes 3

--- MyCenterComputer.Frontend/Program.cs
+++ MyCenterComputer.Frontend/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyCenter.Frontend;
+using MyCenter.Frontend.Repositories;
+using MyCenterComputer.Frontend;
+using MyCenterComputer.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7122/") });
+builder.Services.AddScoped<IRepository,Repository>();
 
 await builder.Build().RunAsync();
