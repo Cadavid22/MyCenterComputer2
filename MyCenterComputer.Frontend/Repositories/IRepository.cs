07bf6b0f39042c4484496bacc0cbf40336bf8ac4
Jorge Cadavid <cadavidjorge10@gmail.com>
2023-06-11 15:14:40 -0500

    Changes 6

--- MyCenterComputer.Frontend/Repositories/IRepository.cs
+++ MyCenterComputer.Frontend/Repositories/IRepository.cs
@@ -1,18 +1,21 @@
 ﻿
 using MyCenter.Shared.Models;
 using MyCenter.Shared.Entities;
+using MyCenterComputer.Frontend.Repositories;
 
 namespace MyCenter.Frontend.Repositories
 {
-    public class IRepository
-    {
-        Task<Response<T>> GetAsync<T>(string url);
+    
+   
+        public interface IRepository
+        {
+            Task<Response<T>> GetAsync<T>(string url);
 
-        Task<Response<T>> PostAsync<T>(string url, T model);
+            Task<Response<T>> PostAsync<T>(string url, T model);
 
-        Task<Response<T>> PutAsync<T>(string url, T model);
+            Task<Response<T>> PutAsync<T>(string url, T model);
 
-        Task<Response<T>> DeleteAsync<T>(string url);
-
+            Task<Response<T>> DeleteAsync<T>(string url);
+        }
     }
-}
+
